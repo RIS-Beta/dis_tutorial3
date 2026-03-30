92737113e7af3d5eec460209d7e322dc73d5d4c0
BorutSavec <bs93205@student.uni-lj.si>
2026-03-30 16:06:10 +0200

    ring normals dont work, people normals do

--- scripts/cluster_people.py
+++ scripts/cluster_people.py
@@ -53,8 +53,6 @@ class Cluster:
         
         self.count += 1
 
-# obstaja možnost da tole ne dela zaradi 2 callback funkcij, mozn rabimo vec threadou
-
 class cluster_people(Node):
     def __init__(self):
         super().__init__('cluster_people')
@@ -130,6 +128,7 @@ class cluster_people(Node):
 
         markerText = self.create_marker(point_stamped, cluster.id, lifetime=0.0, color=(0.0, 0.0, 0.0, 1.0), scale=0.3)
         marker = self.create_marker(point_stamped, cluster.id, lifetime=0.0, color=(1.0, 0.0, 0.0, 0.5), scale=0.7)
+        normal_marker = self.create_normal_arrow_marker(point_stamped, cluster.id + 2000, cluster.normal, arrow_length=0.8, color=(1.0, 0.0, 0.0, 0.9))
         markerText.type = Marker.TEXT_VIEW_FACING
         markerText.id = cluster.id + 1000 
 
@@ -137,7 +136,8 @@ class cluster_people(Node):
 
         markerArray = MarkerArray()
         markerArray.markers.append(marker)
-        markerArray.markers.append(markerText)  
+        markerArray.markers.append(markerText)
+        markerArray.markers.append(normal_marker)
         
         self.marker_pub.publish(markerArray)
         self.get_logger().info(f"Published marker for cluster {cluster.id} of type {cluster.type} with text {marker.text} to /map_goals_marker topic for visualization in rviz")
@@ -172,6 +172,38 @@ class cluster_people(Node):
 
         marker.lifetime = Duration(seconds=lifetime).to_msg()
 
+        return marker
+
+    def create_normal_arrow_marker(self, point_stamped, marker_id, normal_vector, arrow_length=0.8, lifetime=0.0, color=(0.0, 0.0, 1.0, 1.0)):
+        marker = Marker()
+        marker.header = point_stamped.header
+        marker.ns = "cluster_normals"
+        marker.id = marker_id
+        marker.type = Marker.ARROW
+        marker.action = Marker.ADD
+
+        start = Point()
+        start.x = point_stamped.point.x
+        start.y = point_stamped.point.y
+        start.z = point_stamped.point.z
+
+        end = Point()
+        end.x = start.x + normal_vector[0] * arrow_length
+        end.y = start.y + normal_vector[1] * arrow_length
+        end.z = start.z + normal_vector[2] * arrow_length
+
+        marker.points = [start, end]
+
+        marker.scale.x = 0.06
+        marker.scale.y = 0.12
+        marker.scale.z = 0.12
+
+        marker.color.r = color[0]
+        marker.color.g = color[1]
+        marker.color.b = color[2]
+        marker.color.a = color[3]
+
+        marker.lifetime = Duration(seconds=lifetime).to_msg()
         return marker
 
 
